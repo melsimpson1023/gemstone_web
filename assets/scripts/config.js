670f172da959cd54f065bb0d996fca30f8f389cc
melsimpson1023@gmail.com <L0v3ch1ld>
2020-11-12 12:17:25 -0500

    Change link
    change link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://secure-beyond-64741.herokuapp.com/',
+  production: 'https://secure-beyond-64741.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
